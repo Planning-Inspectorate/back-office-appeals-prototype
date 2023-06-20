9ed5b410457e8434de851d30642995d5af690a01
Steve Murphy <murfious@gmail.com>
2023-06-20 11:10:07 +0100

    Adding HAS LPAQ and Moment filters

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,45 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+const moment = require('moment')
+
+addFilter('formatMonth', (number) => {
+	var date = moment().month(number -1)
+	return date.format('MMMM')
+})
+
+addFilter('daysAgo', (number) => {
+	var date = moment().subtract(number,"days").format("D MMMM YYYY")
+	return date
+})
+
+addFilter('daysInFuture', (number) => {
+	var date = moment().add(number,"days").format("D MMMM YYYY")
+	return date
+})
+
+addFilter('daysInFutureShort', (number) => {
+	var date = moment().add(number,"days").format("D MMM YYYY")
+	return date
+})
+
+addFilter('daysInPast', (number) => {
+	var date = moment().subtract(number,"days").format("D MMMM YYYY")
+	return date
+})
+
+addFilter('daysInPastShort', (number) => {
+	var date = moment().subtract(number,"days").format("D MMM YYYY")
+	return date
+})
+
+addFilter('push', (array, item) => {
+	array.push(item)
+	return array
+})
+
+addFilter('cleanArray', (array) => {
+	return array.filter(item => {
+		return (item && (item !==""))
+	})
+})

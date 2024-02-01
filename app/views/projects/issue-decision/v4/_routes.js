8a24eb3bf65c9d98dc4f39f777336de42cf0f12a
Peter Jobes <peter.jobes@kinandcarta.com>
2024-02-01 10:42:27 +0000

    related appeals

---
+++ app/views/projects/issue-decision/v4/_routes.js
@@ -0,0 +1,34 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Manage your appeals'
+
+  next()
+})
+
+router.post('/invalid', function (req, res) {
+  res.redirect('check-your-answers-invalid')
+})
+
+router.post('/check-your-answers-invalid', function (req, res) {
+  res.redirect('confirmation-invalid')
+})
+
+router.post('/upload-decision', function (req, res) {
+  res.redirect('decision-date')
+})
+
+router.post('/decision-date', function (req, res) {
+  res.redirect('check-your-answers')
+})
+
+
+router.post('/check-your-answers', function (req, res) {
+  res.redirect('confirmation')
+})
+
+
+// Add your routes above the module.exports line
+module.exports = router

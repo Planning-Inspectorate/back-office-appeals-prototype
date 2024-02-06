3ae375285a037b139c7dd01c8de838a99425a879
Peter Jobes <peter.jobes@kinandcarta.com>
2024-02-06 17:23:46 +0000

    costs

--- app/views/projects/issue-decision/v4/_routes.js
+++ app/views/projects/issue-decision/v4/_routes.js
@@ -20,14 +20,6 @@ router.post('/upload-decision', function (req, res) {
   res.redirect('decision-date')
 })
 
-router.post('/decision-date', function (req, res) {
-  res.redirect('check-your-answers')
-})
-
-
-router.post('/check-your-answers', function (req, res) {
-  res.redirect('confirmation')
-})
 
 
 // Add your routes above the module.exports line

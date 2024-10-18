299034eab507b7ac77adefb612462c48f492ba6a
Adam Silver <adambsilver@gmail.com>
2024-10-18 15:31:40 +0100

    Separate estimates

--- app/views/projects/start-S78-case/v1/_routes.js
+++ app/views/projects/start-S78-case/v1/_routes.js
@@ -11,10 +11,22 @@ router.post('/add-hearing-details', function (req, res) {
   res.redirect('./add-hearing-details/check')
 })
 
-// router.post('/add-hearing-details/check', function (req, res) {
-//   req.flash('success', 'Hearing details added')
-//   res.redirect('../case-details')
-// })
+router.post('/add-hearing-details/check', function (req, res) {
+  req.flash('success', 'Hearing details added')
+  res.redirect('../case-details')
+})
+
+
+router.post('/add-estimates', function (req, res) {
+  res.redirect('./add-estimates/check')
+})
+
+router.post('/add-estimates/check', function (req, res) {
+  req.flash('success', 'Hearing estimates added')
+  res.redirect('../case-details')
+})
+
+
 
 // Add your routes above the module.exports line
 module.exports = router

80f319dd98f5c22664ce2270400fb7c10d0ff197
Adam Silver <adambsilver@gmail.com>
2024-10-21 13:50:35 +0100

    Fix flash

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,7 @@ const flash = require('connect-flash')
 router.use(flash())
 
 router.all('*', (req, res, next) => {
-  res.locals.flash = req.flash('success')
+  res.locals.flash = req.flash('success')[0]
   next()
 })
 

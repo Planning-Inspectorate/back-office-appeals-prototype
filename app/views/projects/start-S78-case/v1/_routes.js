23f57c0940cbbb4fcb791cb0d5bead596f1677a7
Adam Silver <adambsilver@gmail.com>
2024-10-24 08:41:11 +0100

    Fix edit flow

--- app/views/projects/start-S78-case/v1/_routes.js
+++ app/views/projects/start-S78-case/v1/_routes.js
@@ -7,6 +7,10 @@ router.post('/start-case/check', function (req, res) {
   res.redirect('../case-details')
 })
 
+//
+// HEARING: ADD
+//
+
 router.post('/add-hearing-details', function (req, res) {
   res.redirect('/projects/start-s78-case/v1/add-hearing-details/has-time')
 })
@@ -41,6 +45,47 @@ router.post('/add-hearing-details/check', function (req, res) {
   res.redirect('../case-details')
 })
 
+//
+// HEARING: EDIT
+//
+
+router.post('/edit-hearing-details', function (req, res) {
+  res.redirect('/projects/start-s78-case/v1/edit-hearing-details/has-time')
+})
+
+router.post('/edit-hearing-details/has-time', function (req, res) {
+  if(req.session.data.hearing.hasTime == 'Yes') {
+    res.redirect('/projects/start-s78-case/v1/edit-hearing-details/time')
+  } else {
+    res.redirect('/projects/start-s78-case/v1/edit-hearing-details/has-address')
+  }
+})
+
+router.post('/edit-hearing-details/time', function (req, res) {
+  res.redirect('/projects/start-s78-case/v1/edit-hearing-details/has-address')
+})
+
+router.post('/edit-hearing-details/has-address', function (req, res) {
+  if(req.session.data.hearing.hasAddress == 'Yes') {
+    res.redirect('/projects/start-s78-case/v1/edit-hearing-details/address')
+  } else {
+    res.redirect('/projects/start-s78-case/v1/edit-hearing-details/check')
+  }
+})
+
+router.post('/edit-hearing-details/address', function (req, res) {
+  res.redirect('/projects/start-s78-case/v1/edit-hearing-details/check')
+})
+
+router.post('/edit-hearing-details/check', function (req, res) {
+  req.flash('success', 'Hearing details updated')
+  res.redirect('../case-details')
+})
+
+
+//
+// ESTIMATES: ADD
+//
 
 router.post('/add-estimates', function (req, res) {
   res.redirect('./add-estimates/check')

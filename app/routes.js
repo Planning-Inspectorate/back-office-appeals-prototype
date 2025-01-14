2411668c57bcb2012ee0157c6ee0b6c587b67509
Adam Silver <adambsilver@gmail.com>
2025-01-14 16:56:22 +0000

    Refactor start case

--- app/routes.js
+++ app/routes.js
@@ -14,10 +14,10 @@ router.all('*', (req, res, next) => {
   next()
 })
 
-require('./routes/case-list')(router)
-require('./routes/case-details')(router)
-require('./routes/case-start')(router)
-require('./routes/case-edit-procedure')(router)
+require('./routes/cases')(router)
+require('./routes/case')(router)
+require('./routes/case--start-case')(router)
+require('./routes/case--edit-procedure')(router)
 
 //added for simple branching ++ https://github.com/abbott567/radio-button-redirect ++
 
--- app/routes/case--start-case.js
+++ app/routes/case--start-case.js
@@ -1,24 +1,24 @@
 module.exports = router => {
 
-  router.get('/main/cases/:appealId/start', function (req, res) {
+  router.get('/main/cases/:appealId/start-case', function (req, res) {
     let application = req.session.data.applications.find(application => application.id == req.params.appealId)
-    res.render('/main/cases/start/index', {
+    res.render('/main/cases/start-case/index', {
       application
     })
   })
 
-  router.post('/main/cases/:appealId/start', function (req, res) {
-    res.redirect(`/main/cases/${req.params.appealId}/start/check`)
+  router.post('/main/cases/:appealId/start-case', function (req, res) {
+    res.redirect(`/main/cases/${req.params.appealId}/start-case/check`)
   })
 
-  router.get('/main/cases/:appealId/start/check', function (req, res) {
+  router.get('/main/cases/:appealId/start-case/check', function (req, res) {
     let application = req.session.data.applications.find(application => application.id == req.params.appealId)
-    res.render('/main/cases/start/check', {
+    res.render('/main/cases/start-case/check', {
       application
     })
   })
 
-  router.post('/main/cases/:appealId/start/check', function (req, res) {
+  router.post('/main/cases/:appealId/start-case/check', function (req, res) {
     let application = req.session.data.applications.find(application => application.id == req.params.appealId)
     application.status = 'Ready to validate'
     application.procedure = req.session.data.procedure

45ae0fd9f1efd893a83dd5984a646634b885d46c
Adam Silver <adambsilver@gmail.com>
2025-01-31 12:26:58 +0000

    Timetable

--- app/routes/case--add-procedure.js
+++ app/routes/case--add-procedure.js
@@ -21,7 +21,7 @@ module.exports = router => {
   router.post('/main/cases/:appealId/add-procedure/check', function (req, res) {
     let application = req.session.data.applications.find(application => application.id == req.params.appealId)
     application.procedure = req.session.data.addAppealProcedure.procedure
-    req.flash('success', 'Appeal procedure updated')
+    req.flash('success', 'Appeal procedure added')
     res.redirect(`/main/cases/${req.params.appealId}`)
   })
 

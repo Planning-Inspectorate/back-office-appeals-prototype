b254131c8348ef5ff98b741f20a542ecb9b7dde6
Adam Silver <adambsilver@gmail.com>
2025-03-10 10:10:41 +0000

    Make linking work

--- app/routes/linked-appeals--add.js
+++ app/routes/linked-appeals--add.js
@@ -35,12 +35,19 @@ module.exports = router => {
     let _case = req.session.data.cases.find(_case => _case.id == req.params.caseId)
 
     _case.linkedAppeals.push({
-      id: req.session.data.addLinkedAppeal.id,
+      id: req.session.data.addLinkedAppeal.reference,
       relationship: req.session.data.addLinkedAppeal.relationship
     })
 
     // get the other case and do the opposite so they are linked in both directions
-    // TODO
+    let case2 = req.session.data.cases.find(_case => _case.id == req.session.data.addLinkedAppeal.reference)
+    if(!case2.linkedAppeals) {
+      case2.linkedAppeals = []
+    }
+    case2.linkedAppeals.push({
+      id: _case.id,
+      relationship: req.session.data.addLinkedAppeal.relationship === 'Lead' ? 'Child': 'Lead'
+    })
 
     delete req.session.data.addLinkedAppeal
     req.flash('success', 'Appeal linked')

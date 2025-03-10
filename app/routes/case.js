54157d7cde452a8405f4ff630399a70af764f549
Adam Silver <adambsilver@gmail.com>
2025-03-10 10:02:24 +0000

    show lead/child appeal

--- app/routes/case.js
+++ app/routes/case.js
@@ -1,6 +1,7 @@
 
 const { generateTimetable } = require('../helpers/timetable')
 const { getActions } = require('../helpers/actions')
+const _ = require('lodash')
 
 module.exports = router => {
 
@@ -11,11 +12,21 @@ module.exports = router => {
     let actions = getActions(_case)
     const isCaseStarted = _case.status !== 'Ready to assign case officer' && _case.status !== 'Ready to validate' && _case.status !== 'Ready to start'
 
+    let hasLinkedAppeals = _case.linkedAppeals?.length
+    let isChildAppeal
+    let isLeadAppeal
+    if(hasLinkedAppeals) {
+      isChildAppeal = _.some(_case?.linkedAppeals, { relationship: 'Lead' })
+      isLeadAppeal = _.every(_case?.linkedAppeals, { relationship: 'Child' })
+    }
+
     res.render('/main/cases/show', {
       _case,
       timetable,
       actions,
-      isCaseStarted
+      isCaseStarted,
+      isChildAppeal,
+      isLeadAppeal
     })
   })
 

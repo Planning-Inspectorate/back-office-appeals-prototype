510b2c911690fb667f4c43378acb4839e8ca9d66
Adam Silver <adamsilver@Adams-MBP-2025.lan>
2025-03-24 10:03:44 +0000

    New com flow

--- app/routes/interested-party-comment--add.js
+++ app/routes/interested-party-comment--add.js
@@ -10,30 +10,79 @@ module.exports = router => {
     })
   })
 
-  
+  router.post('/main/appeals/:appealId/interested-party-comments/new', function (req, res) {
+    res.redirect(`/main/appeals/${req.params.appealId}/interested-party-comments/new/has-address`)
+  })
+
+  router.get('/main/appeals/:appealId/interested-party-comments/new/has-address', function (req, res) {
+    let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    res.render('/main/appeals/interested-party-comments/new/has-address', {
+      appeal
+    })
+  })
+
+  router.post('/main/appeals/:appealId/interested-party-comments/new/has-address', function (req, res) {
+    if(req.session.data.addInterestedPartyComment.hasAddress == 'Yes') {
+      res.redirect(`/main/appeals/${req.params.appealId}/interested-party-comments/new/address`)
+    } else {
+      res.redirect(`/main/appeals/${req.params.appealId}/interested-party-comments/new/comment`)
+    }
+  })
+
+  router.get('/main/appeals/:appealId/interested-party-comments/new/address', function (req, res) {
+    let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    res.render('/main/appeals/interested-party-comments/new/address', {
+      appeal
+    })
+  })
+
+  router.post('/main/appeals/:appealId/interested-party-comments/new/address', function (req, res) {
+    res.redirect(`/main/appeals/${req.params.appealId}/interested-party-comments/new/comment`)
+  })
+
+  router.get('/main/appeals/:appealId/interested-party-comments/new/comment', function (req, res) {
+    let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    res.render('/main/appeals/interested-party-comments/new/comment', {
+      appeal
+    })
+  })
+
+  router.post('/main/appeals/:appealId/interested-party-comments/new/comment', function (req, res) {
+    res.redirect(`/main/appeals/${req.params.appealId}/interested-party-comments/new/check`)
+  })
+
+  router.get('/main/appeals/:appealId/interested-party-comments/new/check', function (req, res) {
+    let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    res.render('/main/appeals/interested-party-comments/new/check', {
+      appeal
+    })
+  })
 
   router.post('/main/appeals/:appealId/interested-party-comments/new/check', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    let comment = req.session.data.comment
+    let comment = req.session.data.addInterestedPartyComment
     req.flash('success', 'Interested party comment added')
 
-    // let newParty = {
-    //   id: uuidv4(),
-    //   dateReceived: new Date(),
-    //   emailAddress: party.emailAddress,
-    //   firstName: party.firstName,
-    //   lastName: party.lastName,
-    //   hasOrganisation: party.hasOrganisation,
-    //   organisationName: party.organisationName,
-    //   phone: party.phone,
-    //   status: 'Ready to review'
-    // }
+    let newComment = {
+      id: uuidv4(),
+      dateReceived: new Date(),
+      firstName: comment.firstName,
+      lastName: comment.lastName,
+      emailAddress: comment.emailAddress,
+      hasAddress: comment.hasAddress,
+      address: comment.address,
+      status: 'Ready to review'
+    }
 
-    // if(!appeal.rule6Parties) {
-    //   appeal.rule6Parties = []
-    // }
+    if(!appeal.interestedPartyComments) {
+      appeal.interestedPartyComments = []
+    }
 
-    // appeal.rule6Parties.push(newParty)
+    appeal.interestedPartyComments.push(newComment)
     res.redirect(`/main/appeals/${req.params.appealId}/interested-party-comments`)
   })
 
--- app/routes/interested-party-comment--withdraw.js
+++ app/routes/interested-party-comment--withdraw.js
@@ -2,41 +2,37 @@ const _ = require('lodash')
 
 module.exports = router => {
 
-  router.get('/main/appeals/:appealId/rule-6-parties/:partyId/withdraw', function (req, res) {
+  router.get('/main/appeals/:appealId/interested-party-comments/:commentId/withdraw', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    let party = appeal.rule6Parties.find(party => party.id == req.params.partyId)
+    let comment = appeal.interestedPartyComments.find(comment => comment.id == req.params.commentId)
 
-    res.render('/main/appeals/rule-6-parties/withdraw/index', {
+    res.render('/main/appeals/interested-party-comments/withdraw/index', {
       appeal,
-      party
+      comment
     })
   })
 
-  router.post('/main/appeals/:appealId/rule-6-parties/:partyId/withdraw', function (req, res) {
-    res.redirect(`/main/appeals/${req.params.appealId}/rule-6-parties/${req.params.partyId}/withdraw/check`)
+  router.post('/main/appeals/:appealId/interested-party-comments/:commentId/withdraw', function (req, res) {
+    res.redirect(`/main/appeals/${req.params.appealId}/interested-party-comments/${req.params.commentId}/withdraw/check`)
   })
 
-  router.get('/main/appeals/:appealId/rule-6-parties/:partyId/withdraw/check', function (req, res) {
+  router.get('/main/appeals/:appealId/interested-party-comments/:commentId/withdraw/check', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    let party = appeal.rule6Parties.find(party => party.id == req.params.partyId)
+    let comment = appeal.interestedPartyComments.find(comment => comment.id == req.params.commentId)
 
-    res.render('/main/appeals/rule-6-parties/withdraw/check', {
+    res.render('/main/appeals/interested-party-comments/withdraw/check', {
       appeal,
-      party
+      comment
     })
   })
 
-  router.post('/main/appeals/:appealId/rule-6-parties/:partyId/withdraw/check', function (req, res) {
+  router.post('/main/appeals/:appealId/interested-party-comments/:commentId/withdraw/check', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    let party = appeal.rule6Parties.find(party => party.id == req.params.partyId)
-    if(party.status == 'Ready to review') {
-      req.flash('success', 'Request for Rule 6 status withdrawn')
-    } else {
-      req.flash('success', 'Rule 6 status withdrawn')
-    }
-    party.status = 'Withdrawn'
-    party.dateWithdrawn = new Date()
-    res.redirect(`/main/appeals/${req.params.appealId}/rule-6-parties/${req.params.partyId}`)
+    let comment = appeal.interestedPartyComments.find(comment => comment.id == req.params.commentId)
+    req.flash('success', 'Interested party comment withdrawn')
+    comment.status = 'Withdrawn'
+    comment.dateWithdrawn = new Date()
+    res.redirect(`/main/appeals/${req.params.appealId}/interested-party-comments/${req.params.commentId}`)
   })
 
 }

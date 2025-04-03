6fd851c6a4c4368bdb37919ed0034d79c78ed4e7
Adam Silver <adambsilver@gmail.com>
2025-04-03 11:55:44 +0100

    Move away from table

--- app/routes/linked-appeals.js
+++ app/routes/linked-appeals.js
@@ -1,5 +1,4 @@
-const { getLinkedAppeals } = require('../helpers/linked-appeals')
-// const { isLeadAppeal } = require('../helpers/linked-appeals')
+const { getLinkedAppeals, isLeadAppeal } = require('../helpers/linked-appeals')
 module.exports = router => {
 
   router.get('/main/appeals/:appealId/linked-appeals', function (req, res) {
@@ -7,12 +6,15 @@ module.exports = router => {
     // appeal.isLeadAppeal = isLeadAppeal(appeal.id, req.session.data.linkedAppeals)
 
     let linkedAppeals = getLinkedAppeals(appeal.id, req.session.data.linkedAppeals)
+      .map(item => {
+        let appeal = req.session.data.appeals.find(appeal => appeal.id == item.id)
+        appeal.isLeadAppeal = isLeadAppeal(appeal.id, req.session.data.linkedAppeals)
+        return appeal
+      })
 
     res.render('/main/appeals/linked-appeals/index', {
       appeal,
       linkedAppeals
     })
   })
-
-
 }

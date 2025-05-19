d9ed8afdbd77d9dfff4b2241f1c16e2b89baeeec
Adam Silver <adambsilver@gmail.com>
2025-05-19 16:43:34 +0100

    Update type flows

--- app/routes/edit-type.js
+++ app/routes/edit-type.js
@@ -3,9 +3,13 @@ module.exports = router => {
   router.get('/main/appeals/:appealId/edit-type', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
 
-    res.render('/main/appeals/edit-type/index', {
-      appeal
-    })
+    if(!['Ready to assign case officer', 'Ready to validate', 'Ready to start'].includes(appeal.type)) {
+      res.redirect(`/main/appeals/${req.params.appealId}/edit-type/exit`)
+    } else {
+      res.render('/main/appeals/edit-type/index', {
+        appeal
+      })
+    }
   })
 
   router.post('/main/appeals/:appealId/edit-type', function (req, res) {
@@ -22,7 +26,7 @@ module.exports = router => {
 
   router.post('/main/appeals/:appealId/edit-type/must-resubmit', function (req, res) {
     if(req.session.data.editType.mustResubmit == 'Yes') {
-      res.redirect(`/main/appeals/${req.params.appealId}/edit-type/check`)
+      res.redirect(`/main/appeals/${req.params.appealId}/edit-type/invalid`)
     } else {
       if([
         'Householder', 
@@ -37,6 +41,34 @@ module.exports = router => {
         res.redirect(`/main/appeals/${req.params.appealId}/edit-type/transfer`)
       }
     }
+  })
+
+  router.get('/main/appeals/:appealId/edit-type/exit', function (req, res) {
+    let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    res.render('/main/appeals/edit-type/exit', {
+      appeal
+    })
+  })
+
+  router.get('/main/appeals/:appealId/edit-type/invalid', function (req, res) {
+    let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    res.render('/main/appeals/edit-type/invalid', {
+      appeal
+    })
+  })
+
+  router.get('/main/appeals/:appealId/edit-type/resubmit-date', function (req, res) {
+    let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    res.render('/main/appeals/edit-type/resubmit-date', {
+      appeal
+    })
+  })
+
+  router.post('/main/appeals/:appealId/edit-type/resubmit-date', function (req, res) {
+    res.redirect(`/main/appeals/${req.params.appealId}/edit-type/check`)
   })
 
   router.get('/main/appeals/:appealId/edit-type/transfer', function (req, res) {
@@ -64,8 +96,15 @@ module.exports = router => {
 
   router.post('/main/appeals/:appealId/edit-type/check', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    appeal.type = req.session.data.editType.type
-    req.flash('success', 'Appeal type updated')
+
+    if(req.session.data.editType.mustResubmit == 'Yes') {
+      req.flash('success', 'Appeal marked as invalid')
+      appeal.status = 'Invalid'
+    } else {
+      req.flash('success', 'Appeal type updated')
+      appeal.type = req.session.data.editType.type
+    }
+    
     delete req.session.data.editType
     res.redirect(`/main/appeals/${req.params.appealId}`)
   })

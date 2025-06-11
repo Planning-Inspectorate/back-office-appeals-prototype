e96d9f3a1989c811a8cd7302c8db65aa49673e29
Adam Silver <adambsilver@gmail.com>
2025-06-11 08:49:58 +0100

    Audit

---
+++ app/routes/activity.js
@@ -0,0 +1,11 @@
+module.exports = router => {
+
+  router.get('/main/appeals/:appealId/activity', function (req, res) {
+    let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    res.render('/main/appeals/activity/show', {
+      appeal
+    })
+  })
+
+}

c6f82405c899168897556651b16e05824af453fa
Adam Silver <adambsilver@gmail.com>
2025-06-16 10:36:42 +0100

    Hook up add/edit flows

--- app/routes/residential-units--edit.js
+++ app/routes/residential-units--edit.js
@@ -2,19 +2,39 @@ const _ = require('lodash')
 
 module.exports = router => {
 
-  router.get('/main/appeals/:appealId/appeal-form/residential-units/new', function (req, res) {
+  router.get('/main/appeals/:appealId/appeal-form/residential-units/edit', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
 
-    res.render('/main/appeals/residential-units/index', {
-      appeal
+    let residentialUnits = appeal.appealForm.residentialUnits
+    let netGain = appeal.appealForm.netGain
+    let netLoss = appeal.appealForm.netLoss
+
+    res.render('/main/appeals/appeal-form/residential-units/edit/index', {
+      appeal,
+      residentialUnits,
+      netGain,
+      netLoss
     })
   })
 
-  router.post('/main/appeals/:appealId/appeal-form/residential-units/new', function (req, res) {
+  router.post('/main/appeals/:appealId/appeal-form/residential-units/edit', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    // appeal.appealForm.residentialUnits
-    req.flash('success', 'Number of residential units added')
-    res.redirect(`/main/appeals/${req.params.appealId}`)
+    
+    // Save details
+    appeal.appealForm.residentialUnits = req.session.data.appealForm.residentialUnits
+
+    if(appeal.appealForm.residentialUnits == 'Net gain') {
+      appeal.appealForm.netGain = req.session.data.appealForm.netGain
+    }
+
+    if(appeal.appealForm.residentialUnits == 'Net loss') {
+      appeal.appealForm.netLoss = req.session.data.appealForm.netLoss
+    }
+
+    delete req.session.data.appealForm
+
+    req.flash('success', 'Number of residential units updated')
+    res.redirect(`/main/appeals/${req.params.appealId}/appeal-form`)
   })
 
 }
--- app/routes/residential-units--new.js
+++ app/routes/residential-units--new.js
@@ -12,7 +12,19 @@ module.exports = router => {
 
   router.post('/main/appeals/:appealId/appeal-form/residential-units/new', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    // appeal.appealForm.residentialUnits
+
+    // Save details
+    appeal.appealForm.residentialUnits = req.session.data.appealForm.residentialUnits
+
+    if(appeal.appealForm.residentialUnits == 'Net gain') {
+      appeal.appealForm.netGain = req.session.data.appealForm.netGain
+    }
+
+    if(appeal.appealForm.residentialUnits == 'Net loss') {
+      appeal.appealForm.netLoss = req.session.data.appealForm.netLoss
+    }
+
+    delete req.session.data.appealForm
     req.flash('success', 'Number of residential units added')
     res.redirect(`/main/appeals/${req.params.appealId}/appeal-form`)
   })

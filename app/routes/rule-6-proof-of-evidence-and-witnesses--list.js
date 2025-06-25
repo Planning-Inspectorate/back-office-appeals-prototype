72fc3074236b7d1c8efa0ba46e80d7fca3324c83
Adam Silver <adambsilver@gmail.com>
2025-06-25 15:12:17 +0100

    Old hubs

--- app/routes/rule-6-proof-of-evidence-and-witnesses--list.js
+++ app/routes/rule-6-proof-of-evidence-and-witnesses--list.js
@@ -4,21 +4,17 @@ module.exports = router => {
 
   router.get('/main/appeals/:appealId/rule-6-proof-of-evidence-and-witnesses', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    let proofOfEvidenceAndWitnesses = appeal.rule6Parties.filter((rule6Party) => rule6Party.proofOfEvidenceAndWitnesses)
+    let rule6PartiesWithProofOfEvidenceAndWitnesses = appeal.rule6Parties.filter((rule6Party) => rule6Party.proofOfEvidenceAndWitnesses)
 
-     // Desired status order
-    const statusOrder = ["Ready to review", "Accepted", "Rejected"];
-
-    // Sort based on desired status order
-    proofOfEvidenceAndWitnesses = proofOfEvidenceAndWitnesses.sort((a, b) => {
-        const statusA = a.proofOfEvidenceAndWitnesses.status;
-        const statusB = b.proofOfEvidenceAndWitnesses.status;
-        return statusOrder.indexOf(statusA) - statusOrder.indexOf(statusB);
-    });
+    let proofOfEvidenceAndWitnessesReadyToReview = rule6PartiesWithProofOfEvidenceAndWitnesses.filter(rule6Party => rule6Party.proofOfEvidenceAndWitnesses.status == 'Ready to review')
+    let proofOfEvidenceAndWitnessesAccepted = rule6PartiesWithProofOfEvidenceAndWitnesses.filter(rule6Party => rule6Party.proofOfEvidenceAndWitnesses.status == 'Accepted')
+    let proofOfEvidenceAndWitnessesIncomplete = rule6PartiesWithProofOfEvidenceAndWitnesses.filter(rule6Party => rule6Party.proofOfEvidenceAndWitnesses.status == 'Incomplete')
 
     res.render('/main/appeals/rule-6-proof-of-evidence-and-witnesses/index', {
       appeal,
-      proofOfEvidenceAndWitnesses
+      proofOfEvidenceAndWitnessesReadyToReview,
+      proofOfEvidenceAndWitnessesAccepted,
+      proofOfEvidenceAndWitnessesIncomplete
     })
   })
 

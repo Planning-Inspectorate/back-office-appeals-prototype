6cdd13423ab33aafae1c7cea44977178c5c355e1
Adam Silver <adambsilver@gmail.com>
2025-07-01 09:26:49 +0100

    Add case team flow

--- app/routes/case-team--new.js
+++ app/routes/case-team--new.js
@@ -1,11 +1,22 @@
 const _ = require('lodash')
+const caseTeams = require('../data/case-teams')
 
 module.exports = router => {
 
   router.get('/main/appeals/:appealId/case-team/new', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
+
+    let caseTeamItems = caseTeams.map(caseTeam => ({ 
+      text: caseTeam.name, 
+      value: caseTeam.name, 
+      hint: {
+        text: caseTeam.emailAddress
+      }
+    }))
+
     res.render('/main/appeals/case-team/new/index', {
-      appeal
+      appeal,
+      caseTeamItems
     })
   })
 
@@ -17,16 +28,22 @@ module.exports = router => {
   router.get('/main/appeals/:appealId/case-team/new/check', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
 
+    let newCaseTeam = caseTeams.find(caseTeam => caseTeam.name == req.session.data.addCaseTeam.caseTeam)
+
     res.render('/main/appeals/case-team/new/check', {
-      appeal
+      appeal,
+      newCaseTeam
     })
   })
 
   router.post('/main/appeals/:appealId/case-team/new/check', function (req, res) {
     let appeal = req.session.data.appeals.find(appeal => appeal.id == req.params.appealId)
-    appeal.caseOfficer = req.session.data.addCaseOfficer.caseOfficer
+
+    let newCaseTeam = caseTeams.find(caseTeam => caseTeam.name == req.session.data.addCaseTeam.caseTeam)
+
+    appeal.caseTeam = newCaseTeam
     delete req.session.data.addCaseOfficer
-    req.flash('success', 'Case officer assigned')
+    req.flash('success', 'Case team assigned')
     res.redirect(`/main/appeals/${req.params.appealId}`)
   })
 

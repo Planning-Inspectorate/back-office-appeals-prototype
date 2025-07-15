01f8ad713fac3ab50a06528e783a080a527436af
Adam Silver <adambsilver@gmail.com>
2025-07-15 12:26:51 +0100

    Rule 6 doc status

--- app/routes/appeal.js
+++ app/routes/appeal.js
@@ -29,7 +29,48 @@ module.exports = router => {
       })
 
     const rule6Statements = appeal?.rule6Parties?.map(rule6Party => rule6Party.statement)
-    const rule6ProofOfEvidenceAndWitnesses = appeal?.rule6Parties?.map(rule6Party => rule6Party.rule6ProofsOfEvidenceAndWitnesses)
+    const rule6ProofOfEvidenceAndWitnesses = appeal?.rule6Parties?.map(rule6Party => rule6Party.proofOfEvidenceAndWitnesses)
+
+    const readyCount = rule6Statements.filter(
+      statement => statement?.status === "Ready to review"
+    ).length;
+
+    const acceptedCount = rule6Statements.filter(
+      statement => statement?.status === "Accepted"
+    ).length;
+
+    let rule6StatementsStatus = "No statements";
+    let rule6StatementsLinkText = "View";
+
+    if (readyCount > 0) {
+      rule6StatementsStatus = `${readyCount} statement${readyCount > 1 ? 's' : ''} ready for review`;
+      rule6StatementsLinkText = 'Review'
+    } else if (acceptedCount > 0) {
+      rule6StatementsStatus = `${acceptedCount} statement${acceptedCount > 1 ? 's' : ''}`;
+      rule6StatementsLinkText = 'View'
+    }
+
+    // Proofs
+    const proofsReadyCount = rule6ProofOfEvidenceAndWitnesses.filter(
+      rule6ProofsOfEvidenceAndWitnesses => rule6ProofsOfEvidenceAndWitnesses?.status === "Ready to review"
+    ).length;
+
+    const proofsAcceptedCount = rule6ProofOfEvidenceAndWitnesses.filter(
+      rule6ProofsOfEvidenceAndWitnesses => rule6ProofsOfEvidenceAndWitnesses?.status === "Accepted"
+    ).length;
+
+    let rule6ProofOfEvidenceAndWitnessesStatus = "No proof of evidence and witnesses";
+    let rule6ProofOfEvidenceAndWitnessesLinkText = "View";
+
+    if (proofsReadyCount > 0) {
+      rule6ProofOfEvidenceAndWitnessesStatus = `${proofsReadyCount} proof${proofsReadyCount > 1 ? 's' : ''} of evidence and witnesses ready for review`;
+      rule6ProofOfEvidenceAndWitnessesLinkText = 'Review'
+    } else if (proofsAcceptedCount > 0) {
+      rule6ProofOfEvidenceAndWitnessesStatus = `${proofsAcceptedCount} proof${proofsAcceptedCount > 1 ? 's' : ''} of evidence and witnesses`;
+      rule6ProofOfEvidenceAndWitnessesLinkText = 'View'
+    }
+
+    
 
     const showAddLinkAppeal = canAppealBeLinked(appeal)
 
@@ -41,7 +82,11 @@ module.exports = router => {
       linkedAppeals,
       rule6Statements,
       rule6ProofOfEvidenceAndWitnesses,
-      showAddLinkAppeal
+      showAddLinkAppeal,
+      rule6StatementsStatus,
+      rule6StatementsLinkText,
+      rule6ProofOfEvidenceAndWitnessesStatus,
+      rule6ProofOfEvidenceAndWitnessesLinkText
     })
   })
 

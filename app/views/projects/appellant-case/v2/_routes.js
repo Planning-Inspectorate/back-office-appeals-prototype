880974fcd84d56df543f7ff4e88278bb17e8ce08
Steve Murphy <murfious@gmail.com>
2025-07-23 09:05:55 +0100

    Joining up the S78E start case journey + success banner and using the more simple 'procedure' options version

--- app/views/projects/appellant-case/v2/_routes.js
+++ app/views/projects/appellant-case/v2/_routes.js
@@ -7,7 +7,7 @@ router.get('*', function(req, res, next){
   next()
 })
 
-// Add a case note
+// part 1 check (not used)
 router.post('/part1-check', function (req, res) {
   if (req.session.data['part1check'] == 'No') {
     res.redirect('procedure')
@@ -16,10 +16,19 @@ router.post('/part1-check', function (req, res) {
   }
 })
 
-// Add a case note
+// procedure
 router.post('/procedure', function (req, res) {
   res.redirect('check-your-answers')
 })
+
+// check answers
+router.post('/check-your-answers', function (req, res) {
+  req.flash('success', 'Appeal started')
+  res.redirect('case')
+})
+
+
+
 
 // Add your routes above the module.exports line
 module.exports = router

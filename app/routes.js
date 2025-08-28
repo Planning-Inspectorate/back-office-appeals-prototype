93b62bd42f69a91c988705b111a3104a73dfa9c9
Claire <claire.mcshane@solirius.com>
2025-08-28 10:45:54 +0100

    start date and time added to hearing flow

--- app/routes.js
+++ app/routes.js
@@ -323,3 +323,40 @@ router.get("/projects/closing-cases/v1/change-appeal-resubmit-process", (req, re
 		res.redirect(req.originalUrl.replace("change-appeal-resubmit-process","case-closed"))
 	}
 })
+
+
+
+// NEW HEARING FLOW
+router.post('/enhancements/has-hearing-address', function (req, res) {
+    const answer = req.session.data['startCase']['hasHearingAddress']
+
+    if (answer === 'Yes') {
+      res.redirect('/enhancements/hearing-address.html')   // go to address page
+    } else if (answer === 'No') {
+      res.redirect('/enhancements/check-no-address.html')     // go to check page
+    } else {
+      // if nothing selected, reload the same page with error
+      res.redirect('/enhancements/has-hearing-address')
+    }
+  })
+
+	router.post('/enhancements/hearing-address', function (req, res) {
+	  // data is automatically stored in req.session.data
+	  res.redirect('/enhancements/check')
+	})
+
+	router.post('/enhancements/hearing-date', function (req, res) {
+		// data is automatically stored in req.session.data
+		res.redirect('/enhancements/has-hearing-address')
+	  })
+
+	router.post('/enhancements/start-hearing', function (req, res) {
+	const answer = req.session.data['startCase']['procedure']
+
+	if (answer === 'Hearing') {
+		res.redirect('/enhancements/hearing-date')   // go to hearing date
+	} else {
+		// if nothing selected, reload the same page with error
+		res.redirect('/enhancements/start-hearing')
+	}
+	})

60a75010932a939d8a6b18a87b96f85982c57e4a
Steve Murphy <murfious@gmail.com>
2026-06-16 09:24:56 +0100

    Making a 'flat' version of the expedited case for inspector user research

---
+++ app/views/projects/written-representations-part-1/v2/_routes.js
@@ -0,0 +1,58 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Manage appeals'
+  next()
+})
+
+
+// REVIEW / VALIDATE A CASE
+// ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+router.post('/appellant-case', function (req, res) {
+  if (req.session.data['appelant-outcome'] == 'Valid') {
+    res.redirect('valid-date')
+  } else if (req.session.data['appelant-outcome'] == 'invalid') {
+    res.redirect('why-invalid')
+  } else {
+    res.redirect('incomplete')
+  }
+})
+
+router.post('/valid-date', function (req, res) {
+  req.flash('success', 'Appeal validated')
+  res.redirect('appellant-case?case-stage=ready')
+})
+
+// START A CASE / SET PROCEDURE
+// ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+// part 1 check (not used)
+router.post('/part1-check', function (req, res) {
+  if (req.session.data['part1check'] == 'No') {
+    res.redirect('procedure')
+  } else {
+    res.redirect('check-your-answers')
+  }
+})
+
+// procedure
+router.post('/procedure', function (req, res) {
+  res.redirect('check-your-answers')
+})
+
+// check answers
+router.post('/check-your-answers', function (req, res) {
+  req.flash('success', 'Appeal started')
+  res.redirect('case-details?case-stage=questionnaire')
+})
+
+
+
+// END OF ROUTES
+// ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+// Add your routes above the module.exports line
+module.exports = router
